bc82e6f674b7075bb2b1b0adca6b5971ffaf6c64
Nikola Aleksic <nial147@yahoo.com>
2022-12-01 15:26:36 +0100

    U prvom commit-u za WPF desktop aplikaciju je definisan Model za MVVM.
    U ovom commit-u je ubačen sqLite-net-pcl NuGet package
    - U User modelu su definisani primay key sa autoinkrementacijom i maksimalna dužina stringa za ime i prezime
    - U Notebook modelu je definisan primary key sa autoinkrementacijom za Id kao i [Indexed] atribute za UserId koji će služiti KAO foreign key.
    - U Note modelu je definisan primary key sa autoinkrementacijom za Id kao i [Indexed] atribute za NotebookId
    
    Kreiran je folder ViewModel kao deo MVVM. U njemu je kreiran folder Helpers u kome će biti pomoćne klase.
    U Helpers folderu kreirana je klasa DatabaseHelpers koja će upravljati SQL-om (za sada lokalni SQLite pa onda na Cloud-u)

---
+++ ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,94 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.ViewModel.Helpers
+{
+    public class DatabaseHelper
+    {
+        // Environment.CurrentDirectory pokazuje na lokaciju gde ce se nalaziti nas projekat
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+        // Genericka metoda za ubacivanje bilo kog objekta u svoju tabelu
+        // Ovakva metoda se moze koristiti i za User i za Note i za Notebook
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+
+            // using statement je da se kreira konekcija, obavljaju potrebne radnje i na kraju zatvori conn konekcija
+            using(SQLiteConnection conn= new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+
+                //Insert metoda je tipa int koja vraca broj dodatih redova u tabeli.
+                //Ako je kreiran neki red onda znaci da result treba da postane true
+                int rows = conn.Insert(item);
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+
+                //Update metoda je tipa int koja vraca broj azuriranih redova u tabeli
+                int rows = conn.Update(item);
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+
+        public static bool Delete<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+
+                //Delete metoda je tipa int koja vraca broj izbrisanih redova u tabeli
+                int rows = conn.Delete(item);
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+
+        public static List<T> Read<T>() where T : new()
+        {
+            List<T> items;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                items = conn.Table<T>().ToList();
+            }
+
+            return items;
+        }
+
+    }
+}
