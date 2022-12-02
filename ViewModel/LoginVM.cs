195f7877429598fbe34645f82e0077f0a2a2a849
Nikola Aleksic <nial147@yahoo.com>
2022-12-02 22:47:21 +0100

    Izbrisan Main Window a napravljen je folder View, koji je deo MVVM, gde ce biti: - LoginWindow - NotesWindow
    Acsess modifier za klase u Model folderu su promenjene sa internal u public da mozemo da im pristupamo
    
    Komande ce koristiti interfejs ICommand.
    Dodat je folder Commands i klase:
    - LoginCommand  - izvrsice se kada korisnik klikne na dugme Login
    - RegisterCommand - izvrsice se kada korisnik klikne na dugme Regiser
    - NewNotebookCommand
    - NewNoteCommand
    
    U postojecem folderu ViewModel dodate su klase:
    - LoginVM - sluzice nam za Binding za Login Window
    - NotesVM - ovde cemo izlistati Notebook-ove, Notes, ispisace tekst u fajlovima i trebace nam kreacija Notebook-a i Notes-a

---
+++ ViewModel/LoginVM.cs
@@ -0,0 +1,31 @@
+﻿using EvernoteClone.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace EvernoteClone.ViewModel
+{
+    public class LoginVM
+    {
+		private User user;
+		public User User
+		{
+			get { return user; }
+			set { user = value; }
+		}
+
+		public RegisterCommand RegisterCommand { get; set; }
+		public LoginCommand LoginCommand { get; set; }
+
+		// konstruktor
+		public LoginVM()
+		{
+			// prosledjujemo this jer konstruktor za nasu RegisterCommand klasu sada zahteva LoginVM instancu
+			RegisterCommand = new RegisterCommand(this);
+			LoginCommand = new LoginCommand(this);
+		}
+	}
+}
---
+++ ViewModel/NotesVM.cs
@@ -0,0 +1,40 @@
+﻿using EvernoteClone.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace EvernoteClone.ViewModel
+{
+    public class NotesVM
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
+		private Notebook selectedNotebook;
+		public Notebook SelectedNotebook
+		{
+			get { return selectedNotebook; }
+			set 
+			{ 
+				selectedNotebook = value;
+				//TODO: get notes
+			}
+		}
+
+		// dobicemo Notes svaki put kada se SelectedNotebook promeni i sacuvacemo ih u ovoj ObservableCollection
+		public ObservableCollection<Note> Notes { get; set; }
+
+		public NewNotebookCommand NewNotebookCommand { get; set; }
+		public NewNoteCommand NewNoteCommand { get; set; }
+
+		// konstruktor
+		public NotesVM()
+		{
+			NewNotebookCommand = new NewNotebookCommand(this);
+			NewNoteCommand = new NewNoteCommand(this);
+		}
+	}
+}
