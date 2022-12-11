af109c76e1d005b36c314fbe76d6b04420c2c1f3
Nikola Aleksic <nial147@yahoo.com>
2022-12-11 20:09:01 +0100

    Gogle Firebase Services:
    - Odradjena Execute funkcionalnost (koja je ranije imala samo komentar TODO) u LoginComand.cs i RegisterCommand.cs klasi.
    - Zavrsen LoginWindow.xaml
    - VM preimenovan u ViewModel u RegisterCommand.cs i LoginCommand.cs
    - Promenjen Namespace u EvernoteClone

--- ViewModel/Commands/LoginCommand.cs
+++ ViewModel/Commands/LoginCommand.cs
@@ -4,28 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using EvernoteClone.Model;
 
 namespace EvernoteClone.ViewModel.Commands
 {
     public class LoginCommand : ICommand
     {
-        public LoginVM VM { get; set; }
+        public LoginVM ViewModel { get; set; }
 
-        public event EventHandler? CanExecuteChanged;
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
 
         public LoginCommand(LoginVM vm)
         {
-            VM = vm;
+            ViewModel = vm;
         }
 
-        public bool CanExecute(object? parameter)
+        public bool CanExecute(object parameter)
         {
+            User user = parameter as User;
+
+            if (user == null) return false;
+            if (string.IsNullOrEmpty(user.Username)) return false;
+            if (string.IsNullOrEmpty(user.Password)) return false;
             return true;
         }
 
         public void Execute(object? parameter)
         {
-            //TODO Login functionality
+            ViewModel.Login();
         }
     }
 }
